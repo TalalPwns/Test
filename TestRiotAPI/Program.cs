7cc7448d1fe921e5212703262bb8f64d6c2f41cf
Talal Azhar Uddin <TalalAzhar@gmail.com>
2020-11-06 00:38:50 -0500

    Add project files.

---
+++ TestRiotAPI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestRiotAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
